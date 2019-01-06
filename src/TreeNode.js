e8b3e877471bdb52f8ca34191de35c593e436e2c
xiangwei71 <xiangwei71@gmail.com>
2019-01-06 20:06:15 +0800

    drop後同步TreeNode資料

--- src/TreeNode.js
+++ src/TreeNode.js
@@ -1,9 +1,19 @@
+var DraggableTree_hash = null;
+
 function TreeNode(id) {
   this.id = id;
+  this.parent = null;
   this.childs = [];
 
   this.addChild = function (node) {
+    node.parent = this;
     this.childs.push(node);
+  }
+
+  this.removeChild = function (node) {
+    node.parent = null;
+    var index = this.childs.indexOf(node);
+    this.childs.splice(index, 1);
   }
 
   this.render = function () {
@@ -21,10 +31,31 @@ function TreeNode(id) {
               </div> \
             </div > ".replace(/@id/g, id).replace("@content", content);
   }
+
+  this.DebugStr = function (depth) {
+    console.log(getSpace(depth) + this.id)
+    ++depth;
+
+    this.childs.forEach(element => {
+      element.DebugStr(depth);
+    });
+  }
+}
+
+function getSpace(depth) {
+  var space = "";
+  for (var i = 0; i < depth; ++i)
+    space += "  ";
+
+  return space;
 }
 
 function buildTree(obj) {
   var node = new TreeNode(obj.name);
+
+  if (DraggableTree_hash == null)
+    DraggableTree_hash = {};
+  DraggableTree_hash[obj.name] = node;
   for (var i = 0; i < obj.childs.length; ++i) {
     var element = obj.childs[i];
     node.addChild(buildTree(element));
@@ -44,7 +75,6 @@ function DraggableTree_dragOver(ev) {
 }
 
 function DraggableTree_dragOut(ev) {
-  // ev.preventDefault();
   document.getElementById(ev.target.id).className = "droppable";
 }
 
@@ -54,18 +84,19 @@ function DraggableTree_dragStart(ev) {
 
 function DraggableTree_drop(ev) {
   ev.preventDefault();
+
+  // 取消框線
   document.getElementById(ev.target.id).className = "droppable";
 
-  var draggable_id = ev.dataTransfer.getData("text").replace("_draggable", "");
-  var droppable_id = ev.target.id.replace("_droppable", "")
-  console.log(draggable_id + " to " + droppable_id);
+  var ui_draggable_id = ev.dataTransfer.getData("text").replace("_draggable", "");
+  var ui_droppable_id = ev.target.id.replace("_droppable", "")
 
-  var dropTarget = document.getElementById(droppable_id);
-  var dragTarget = document.getElementById(draggable_id);
-  var content = document.getElementById(droppable_id + "_content");
+
+  var dragTarget = document.getElementById(ui_draggable_id);
+  var content = document.getElementById(ui_droppable_id + "_content");
 
   // 擋掉: cannot set parent to child
-  if (DraggableTree_findDomById(dragTarget, droppable_id)) {
+  if (DraggableTree_findDomById(dragTarget, ui_droppable_id)) {
     alert("cannot set parent to child");
     return;
   }
@@ -75,6 +106,15 @@ function DraggableTree_drop(ev) {
 
   // 打開
   content.className = "open";
+
+  //更新TreeNode
+  var draggable_id = ui_draggable_id.replace("treeNode_", "");
+  var droppable_id = ui_droppable_id.replace("treeNode_", "");
+  console.log(draggable_id + " to " + droppable_id);
+  DraggableTree_hash[draggable_id].parent.removeChild(DraggableTree_hash[draggable_id]);
+  DraggableTree_hash[droppable_id].addChild(DraggableTree_hash[draggable_id]);
+
+  DraggableTree_hash['root'].DebugStr(0);
 }
 
 function DraggableTree_findDomById(dom, id) {
